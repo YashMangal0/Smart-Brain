25872a2a5285716c6469f00060c90d31cb809753
Yash mangal <84276987+YashMangal0@users.noreply.github.com>
2024-01-13 00:25:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const db= knex ({
       host : '127.0.0.1',
       port : 5432,
       user : 'postgres',
-      password : 'yash',
+      password : 'YOUR PASSWORD',
       database : 'smart-brain'
     }
   });

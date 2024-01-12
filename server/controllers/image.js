3efb292418de2670d2c2ef920bccf0ae948f4bd2
Yash mangal <84276987+YashMangal0@users.noreply.github.com>
2024-01-13 00:26:56 +0530

    Update image.js

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -1,36 +1,5 @@
-const returnClarifaiRequest=(imageUrl)=>{
-  const PAT = '2f9964144a8c410f9c07bf8edf60e9ba';
-  const USER_ID = 't1c533y5fyara';       
-  const APP_ID = 'my-first-application-a6in7w';
-  const MODEL_ID = 'face-detection';
-  const IMAGE_URL = imageUrl;
-  
-  const raw = JSON.stringify({
-   "user_app_id": {
-       "user_id": USER_ID,
-       "app_id": APP_ID
-   },
-   "inputs": [
-       {
-           "data": {
-               "image": {
-                   "url": IMAGE_URL
-               }
-           }
-       }
-   ]
-  });
-  return {
-   method: 'POST',
-   headers: {
-       'Accept': 'application/json',
-       'Authorization': 'Key ' + PAT
-   },
-   body: raw
-  };
-  }
 const handleApiCall=(req,res)=>{
-  fetch("https://api.clarifai.com/v2/models/" + 'face-detection'  + "/outputs", returnClarifaiRequest(req.body.input))
+  fetch("https://api.clarifai.com/v2/models/" + 'face-detection'  + "/outputs",req.body.input)
   .then(data=>{
     res.json(data);
   })
